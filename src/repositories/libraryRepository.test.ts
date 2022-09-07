a15aa6141cf809e66c6c753fd53b720dae684cf1
Mathieson Sterling <mathieson@cherre.com>
2022-09-07 14:20:11 -0400

    feat: add more tests and mocking for library retrieval

--- src/repositories/libraryRepository.test.ts
+++ src/repositories/libraryRepository.test.ts
@@ -1,16 +1,35 @@
-import {Borrower, Person, PersonName, SimpleLibrary} from "@meansofproduction/domain";
-import {LibraryRepository} from "./libraryRepository";
+import {
+    Person,
+    PersonName,
+    ILibrary,
+    IBorrower,
+} from "@meansofproduction/domain"
+import {LibraryRepository} from "./libraryRepository"
+import {instance, mock, when} from "ts-mockito";
 
 
 describe("library repository", () => {
-    it("gets libraries via person", () => {
-        const person = new Person("test", new PersonName("Testy", "McTesterson"), [])
+    it("gets libraries via person", () => {1
+        const personName = new PersonName("Testy", "McTesterson")
+        const person = new Person("test", personName, [])
 
-        const underTest = new LibraryRepository()
+        const mockLib1: ILibrary = mock()
+
+        const mockBorrower1: IBorrower = mock()
+        when(mockBorrower1.person).thenReturn(person)
+        when(mockLib1.borrowers).thenReturn([instance(mockBorrower1)])
+
+        const mockLib2: ILibrary = mock()
+        const mockBorrower2: IBorrower = mock()
+        when(mockBorrower2.person).thenReturn(new Person("nope", personName, []))
+        when(mockLib2.borrowers).thenReturn([instance(mockBorrower2)])
+
+        const underTest = new LibraryRepository([instance(mockLib1), instance(mockLib2)])
 
         // act
-        const res = underTest.getLibrariesPersonCanUse(person)
+        const res = Array.from(underTest.getLibrariesPersonCanUse(person))
 
         expect(res).not.toBeNull()
+        expect(res.length).toEqual(1)
     })
 })
