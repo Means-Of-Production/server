ab2b111a53938c9a6095db21d2ec91463e70f2cb
Matty Roses <debsnroses@protonmail.com>
2022-09-08 13:57:38 -0400

    Remove stuff for domain

--- src/services/IThingSearchService.ts
+++
@@ -1,6 +0,0 @@
-import {IThing, Person, ThingTitle} from "@meansofproduction/domain";
-import {SearchRequest} from "../valueItems/searchRequest"
-
-export interface IThingSearchService {
-    find(person: Person, searchRequest: SearchRequest): Iterable<ThingTitle>
-}
--- src/valueItems/searchRequest.ts
+++
@@ -1,7 +0,0 @@
-export class SearchRequest {
-    readonly name: string | undefined
-
-    constructor(name: string | undefined) {
-        this.name = name
-    }
-}
