75455fa3b9f792871a40149abdef08923c49a2a2
Mathieson Sterling <mathieson@cherre.com>
2022-09-09 09:03:56 -0400

    feat: add item to verify item works!

--- src/context.ts
+++ src/context.ts
@@ -1,32 +1,55 @@
 import {LibraryRepository} from "./repositories/libraryRepository"
 import {
-    EmailAddress, ILibraryRepository,
+    EmailAddress,
+    ILibraryRepository,
     MoneyFactory,
     Person,
     PersonName,
     PhysicalLocation,
-    SimpleLibrary, SimpleTimeBasedFeeSchedule,
+    SimpleLibrary,
+    SimpleTimeBasedFeeSchedule,
+    Thing,
+    ThingStatus,
+    ThingTitle,
     USDMoney,
     WaitingListFactory
 } from "@meansofproduction/domain"
 
 const moneyFactory = new MoneyFactory()
 
+const simpleLibrary =         new SimpleLibrary(
+    "testLib1",
+    "testLibrary",
+    new Person("admin", new PersonName("Testy", "McTesterson"), [new EmailAddress("testy@test.com")]),
+    new PhysicalLocation(0, 0),
+    new WaitingListFactory(false),
+    new USDMoney(100),
+    [],
+    moneyFactory,
+    new SimpleTimeBasedFeeSchedule(new USDMoney(10), moneyFactory)
+)
+
 const libraryRepository = new LibraryRepository(
     [
-        new SimpleLibrary(
-            "testLib1",
-            "testLibrary",
-            new Person("admin", new PersonName("Testy", "McTesterson"), [new EmailAddress("testy@test.com")]),
-            new PhysicalLocation(0, 0),
-            new WaitingListFactory(false),
-            new USDMoney(100),
-            [],
-            moneyFactory,
-            new SimpleTimeBasedFeeSchedule(new USDMoney(10), moneyFactory)
-        )
+        simpleLibrary
     ]
 )
+
+const tableSaw = new Thing(
+    "847s77fhd",
+    new ThingTitle(
+        "Ryobi Table Saw",
+        "",
+        "295848321"
+    ),
+    simpleLibrary.location,
+    simpleLibrary,
+    ThingStatus.READY,
+    "A nice table saw portable for job use",
+    ["https://homedepot.scene7.com/is/image/homedepotcanada/p_1001098919.jpg?wid=1000&hei=1000&op_sharpen=1"],
+    null,
+)
+simpleLibrary.addItem(tableSaw)
 
 export interface Context{
     libraryRepository: ILibraryRepository
